89bf9f9a025b63eed4de5154382641766ac391dd
Christian Fei <crifei93@gmail.com>
2018-06-15 22:43:10 +0200

    cleanup + list members 👤

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const key = process.env.TRELLO_API_KEY || process.env.npm_config_TRELLO_API_KEY
 const token = process.env.TRELLO_API_TOKEN || process.env.npm_config_TRELLO_API_TOKEN
 
 if (!boardName) {
-  process.stdout.write('Please provide a valid board name')
+  process.stdout.write(`Please provide a valid board name`)
   process.exit(1)
 }
 
@@ -21,26 +21,28 @@ async function main (boardName, key, token) {
   process.stdout.write(`Loading cards for ${boardName}`)
   const boards = await getBoards({key, token})
   const board = boards.find(b => b.name === boardName)
+
   if (!board) {
     process.stdout.write('could not find board')
-    process.stdout.write(`available boards are ${boards.map(b => b.name).join('\n')}`)
+    process.stdout.write(`available boards are:\n ${boards.map(b => b.name).join('\n ')}`)
   }
 
+  const members = await getBoardMembers({key, token}, board.id)
   const lists = await getBoardLists({key, token}, board.id)
-  const listsById = lists.reduce((acc, list) => Object.assign(acc, {
-    [list.id]: list
-  }), {})
   const listsSorted = lists.sort((l1, l2) => l1.pos - l2.pos)
 
-  const cards = await getBoardCards({key, token}, board.id, since)
-  const cardsWithList = cards.map(c => Object.assign(c, {
-    list: listsById[c.idList]
+  let cards = await getBoardCards({key, token}, board.id, since)
+  cards = cards.map(c => Object.assign(c, {
+    list: lists.find(l => l.id === c.idList),
+    members: members.filter(m => c.idMembers.includes(m.id))
   }))
-  const cardsPerList = cardsWithList.reduce((acc, curr) => Object.assign(acc, {
+
+  const cardsPerList = cards.reduce((acc, curr) => Object.assign(acc, {
     [curr.idList]: (acc[curr.idList] || []).concat([curr])
   }), {})
 
   for (const list of listsSorted) {
+    if (!cardsPerList[list.id]) continue
     process.stdout.write(listToString(list))
     for (const card of cardsPerList[list.id]) {
       process.stdout.write(cardToString(card))
@@ -57,6 +59,12 @@ function getBoards ({key, token}) {
 function getBoardLists ({key, token}, boardId) {
   let url = `https://api.trello.com/1/boards/${boardId}/lists?key=${key}&token=${token}`
   log('-> getBoardLists', boardId)
+  return get(url, {json: true}).then(r => r.body)
+}
+
+function getBoardMembers ({key, token}, boardId) {
+  let url = `https://api.trello.com/1/boards/${boardId}/members?key=${key}&token=${token}`
+  log('-> getBoardMembers', boardId)
   return get(url, {json: true}).then(r => r.body)
 }
 
@@ -77,8 +85,11 @@ function getBoardCards ({key, token}, boardId, since) {
 
 function cardToString (card) {
   return [
-    `⚡️   ${card.name}`,
+    `⚡️        ${card.name}`,
+    `\n    🔗    ${card.url}`,
     card.labels.length > 0 ? `\n    ${labelsToString(card.labels)}` : false,
+    card.members.length > 0 ? `\n    ${membersToString(card.members)}` : false,
+    `\n`,
     `\n`
   ]
   .filter(Boolean)
@@ -86,9 +97,15 @@ function cardToString (card) {
 }
 
 function labelsToString (labels) {
-  return `labels: ${labels.map(l => l.name).join(', ')}`
+  return `🏷    ${labels.map(l => l.name).join(', ')}`
+}
+
+function membersToString (members) {
+  return `👤    ${members.map(m => m.fullName).join(', ')}`
 }
 
 function listToString (list) {
-  return `\n📋   "${list.name}" (${list.id})\n`
+  return `\n📋   "${list.name}"\n`
 }
+
+process.on('unhandledRejection', console.error)
