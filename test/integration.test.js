6468109ba33a93087b0a20342faeb24c334567b9
irene nicolini <irene@MacBook-Pro-di-irene.local>
2019-02-17 11:58:46 +0100

    test(filter-by-label): add integration test to filter cards by label

--- test/integration.test.js
+++ test/integration.test.js
@@ -1,8 +1,8 @@
 /* globals test */
 const { equal, ok } = require('assert')
 const main = require('..')
-
 const boardName = 'trello-recap-integration-tests'
+const member = 'babba_regina' // 'christianfei1'
 
 test('loads cards, lists and members from board', async (done) => {
   const key = process.env.TRELLO_API_KEY || process.env.npm_config_TRELLO_API_KEY
@@ -20,7 +20,6 @@ test('loads cards, lists and members from board', async (done) => {
 test('filters cards by member', async (done) => {
   const key = process.env.TRELLO_API_KEY || process.env.npm_config_TRELLO_API_KEY
   const token = process.env.TRELLO_API_TOKEN || process.env.npm_config_TRELLO_API_TOKEN
-  const member = 'christianfei1'
   const {cards} = await main({key, token}, {boardName, member})
   ok(cards)
   equal(cards.length, 1)
@@ -34,5 +33,15 @@ test('filters lists by name', async (done) => {
   const {lists} = await main({key, token}, {boardName, listName})
   ok(lists)
   equal(lists.length, 1)
+  done()
+})
+
+test('filters cards by label', async (done) => {
+  const key = process.env.TRELLO_API_KEY || process.env.npm_config_TRELLO_API_KEY
+  const token = process.env.TRELLO_API_TOKEN || process.env.npm_config_TRELLO_API_TOKEN
+  const label = 'Bug'
+  const {cards} = await main({key, token}, {boardName, label})
+  ok(cards)
+  equal(cards.length, 2)
   done()
 })
