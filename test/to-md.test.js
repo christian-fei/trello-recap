fb10d14a0fbfa0272ec27eb55d9ad3d947937c1e
Christian Fei <crifei93@gmail.com>
2019-02-23 21:20:31 +0100

    lint

--- test/to-md.test.js
+++ test/to-md.test.js
@@ -1,18 +1,18 @@
 /* globals test */
-const { equal } = require('assert')
+const assert = require('assert')
 const toMD = require('../lib/to-md')
 
 test('to-md: h1', () => {
-  const md = toMD({type: 'h1', text: 'text'})
-  equal(md, '# text')
+  const md = toMD({ type: 'h1', text: 'text' })
+  assert.strictEqual(md, '# text')
 })
 
 test('to-md: h2', () => {
-  const md = toMD({type: 'h2', text: 'text'})
-  equal(md, '## text')
+  const md = toMD({ type: 'h2', text: 'text' })
+  assert.strictEqual(md, '## text')
 })
 
 test('to-md: regular text', () => {
-  const md = toMD({text: 'text'})
-  equal(md, 'text')
+  const md = toMD({ text: 'text' })
+  assert.strictEqual(md, 'text')
 })
